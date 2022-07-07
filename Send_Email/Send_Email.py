a6de72bbab5f2c6cb495a6ccc87f5963fd748eb7
onifaie <onifaie@gmail.com>
2022-07-08 00:54:11 +0300

    Traking_Jop

---
+++ Send_Email/Send_Email.py
@@ -0,0 +1,19 @@
+import smtplib, ssl
+
+def send_email(message):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "onifaie@gmail.com"
+    receiver_email = "onifaie2@gmail.com"
+    password = "npycppygwnwmahzb"
+    
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        try:
+            server.login(sender_email, password)
+            res = server.sendmail(sender_email, receiver_email, message)
+            print('email sent!')
+        except:
+            print("could not login or send the mail.")
+            
+# send_email("hello this come from obeid 202")
